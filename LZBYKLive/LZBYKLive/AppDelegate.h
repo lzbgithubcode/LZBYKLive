e3c8ab86876930e8ff2449b77ea1df9e6609cb1d
apple <apple@platonademacbook-pro.local>
2016-08-04 15:55:53 +0800

    初始化

---
+++ LZBYKLive/LZBYKLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZBYKLive
+//
+//  Created by apple on 16/8/4.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
